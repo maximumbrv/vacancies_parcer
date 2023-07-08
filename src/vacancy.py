a4417487414f30f2b9403c175fa7e079e2946e9b
Maxim Bryzgalov <maximumbrv@yandex.ru>
2023-07-08 17:33:46 +0300

    implement __init__ in Vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,5 +1,8 @@
-class Vacancy():
+class Vacancy:
     vacancies = []
 
-    def __init__(self):
-        pass
+    def __init__(self, name, url, salary, description):
+        self.name = name
+        self.url = url
+        self.salary = salary
+        self.description = description
