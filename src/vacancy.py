ace98f75ed307ed3180b65b4c99d3a22a5dff897
Maxim Bryzgalov <maximumbrv@yandex.ru>
2023-07-08 18:53:09 +0300

    add instantiate_from_hh_list method

--- src/vacancy.py
+++ src/vacancy.py
@@ -8,3 +8,30 @@ class Vacancy:
         self.description = description
         self.platform = platform
         self.vacancies.append(self)
+
+    @classmethod
+    def instantiate_from_hh_list(cls, vacancies_list):
+        for vacancy in vacancies_list:
+            name = vacancy['name']
+            url = vacancy['url']
+
+            if vacancy['salary']:
+                salary = {'from': vacancy['salary']['from'], 'to': vacancy['salary']['to']}
+            else:
+                salary = None
+
+            snippet = vacancy['snippet']
+            if snippet:
+                if snippet['requirement'] is None:
+                    del snippet['requirement']
+                if snippet['responsibility'] is None:
+                    del snippet['responsibility']
+                description = '\n'.join(snippet.values())
+            else:
+                description = None
+
+            platform = 'hh'
+
+            cls.vacancies.append(cls(name, url, salary, description, platform))
+
+
