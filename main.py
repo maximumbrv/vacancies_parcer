73778dc5f34c2d7c76e0a0eea1e145b06958c73c
Maxim Bryzgalov <maximumbrv@yandex.ru>
2023-07-08 19:15:12 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from src.api import HeadHunterAPI
+from src.api import HeadHunterAPI, SuperJobApi
 from src.vacancy import Vacancy
 
 
 def user_interaction():
     hh = HeadHunterAPI()
     hh.get_vacancies('Python')
-    print(hh.to_list()[0])
+    sj = SuperJobApi()
+    sj.get_vacancies('Python')
+    Vacancy.instantiate_from_sj_list(sj.vacancies)
+    for vac in Vacancy.vacancies:
+        print(repr(vac))
+        print()
+
 
 
 if __name__ == '__main__':
