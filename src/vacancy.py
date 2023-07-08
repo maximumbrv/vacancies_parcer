359babf9b5fc6ad744614e2899cd5c280ce3f67f
Maxim Bryzgalov <maximumbrv@yandex.ru>
2023-07-08 18:11:52 +0300

    modify __init__ in Vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,8 +1,10 @@
 class Vacancy:
     vacancies = []
 
-    def __init__(self, name, url, salary, description):
+    def __init__(self, name, url, salary, description, platform=None):
         self.name = name
         self.url = url
         self.salary = salary
         self.description = description
+        self.platform = platform
+        self.vacancies.append(self)
