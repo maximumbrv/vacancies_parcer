883acd477559bf4519dc702751704c814fc58c37
Maxim Bryzgalov <maximumbrv@yandex.ru>
2023-07-08 20:21:06 +0300

    modify Saver abstract class

--- src/saver.py
+++ src/saver.py
@@ -3,6 +3,23 @@ from abc import ABC, abstractmethod
 
 
 class Saver(ABC):
+
+    @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self):
+        pass
+
+    @abstractmethod
+    def open(self):
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
+
     @abstractmethod
     def get_vacancies(self):
         pass
@@ -12,5 +29,20 @@ class Saver(ABC):
         pass
 
     @abstractmethod
+    def add_vacancies(self, *args):
+        pass
+
+
+class JSONSaver(Saver):
+
+    def __init__(self, path='vacancies.json'):
+        pass
+
+    def get_vacancies(self):
+        pass
+
+    def get_vacancies_by_keyword(self, keyword):
+        pass
+
     def add_vacancies(self, *args):
         pass
